56d344e33c24946d924a6315a209717ae8d7dfe8
Donn603 <82605889+Donn603@users.noreply.github.com>
2025-03-09 18:59:57 +0200

    Add ExpenseTracker/Models/TransactionDetailsModel.cs 1741539597626

---
+++ ExpenseTracker/Models/TransactionDetailsModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class TransactionDetailsModel
+    {
+
+        [Key]
+        public int TransactionDetailId { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int TransactionId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [MaxLength(200)]
+        public string Notes { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation properties
+        public TransactionsModel Transaction { get; set; }
+        public CategoriesModel Category { get; set; }
+    }
+}
