6f92007198b05329cfb7022da9537c8ef79f38d2
Donn603 <82605889+Donn603@users.noreply.github.com>
2025-03-09 18:59:54 +0200

    Add ExpenseTracker/Models/UsersModel.cs 1741539594472

---
+++ ExpenseTracker/Models/UsersModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Expense_Tracker.Models
+{
+    public class UsersModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<TransactionsModel> Transactions { get; set; }
+        public ICollection<CategoriesModel> Categories { get; set; }
+        public ICollection<VendorsModel> Vendors { get; set; }
+    }
+}
