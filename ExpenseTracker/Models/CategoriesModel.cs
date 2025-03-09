1f998e6da212565a6b1ce3da291ac03fea3091a9
Donn603 <82605889+Donn603@users.noreply.github.com>
2025-03-09 19:00:00 +0200

    Add ExpenseTracker/Models/CategoriesModel.cs 1741539600390

---
+++ ExpenseTracker/Models/CategoriesModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class CategoriesModel
+    {
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; }
+
+        // Navigation properties
+        public UsersModel User { get; set; }
+        public ICollection<TransactionDetailsModel> TransactionDetails { get; set; }
+    
+    }
+}
