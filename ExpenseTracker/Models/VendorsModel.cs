299353e58d96e965c8f9ebacf6a7ee395da88192
Donn603 <82605889+Donn603@users.noreply.github.com>
2025-03-09 18:59:53 +0200

    Add ExpenseTracker/Models/VendorsModel.cs 1741539593123

---
+++ ExpenseTracker/Models/VendorsModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class VendorsModel
+    {
+
+        [Key]
+        public int VendorId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; }
+
+        // Navigation properties
+        public UsersModel User { get; set; }
+        public ICollection<TransactionsModel> Transactions { get; set; }
+    }
+}
